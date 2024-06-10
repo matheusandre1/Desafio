040c20948c9bcd22729565fc3f6430d0fe277c21
Matheus André <92062874+matheusandre1@users.noreply.github.com>
2024-06-10 19:53:16 -0300

    Corrigido

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-for i in range(21,0,-1):
+for i in range(20,1-1,-1):
     if i == 13:
         continue;   
     print(i);
@@ -6,10 +6,9 @@ for i in range(21,0,-1):
 
 numero = 20;
 
-while numero > 1:
+while numero >= 1:
     if numero != 13:
        print(numero)
     numero -=1;
         
-
 
